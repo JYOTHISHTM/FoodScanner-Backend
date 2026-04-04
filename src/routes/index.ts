edaa2aefa5b5bb6b525c6c3abaf24161ac20412f
Jyothish T M <jothishtm2002@gmail.com>
2026-04-04 17:46:03 +0530

    feat: add scan routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,15 @@ import { Router } from "express";
 import authRoutes from "../modules/auth/interface/auth.routes";
 import productRoutes from "../modules/product/interface/product.routes";
 import userProfile from "../modules/user/interface/user.routes"
+import scanRoutes from "../modules/scan/interface/scan.routes";
+
+
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/product", productRoutes);
 router.use("/profile",userProfile)
+router.use("/scans", scanRoutes);
+
 
 export default router;
