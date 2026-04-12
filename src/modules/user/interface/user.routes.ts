a9c53e71dc46593ccf6d906eca88e4decd81dfa2
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-12 19:45:53 +0530

    feat: protect user routes with auth middleware

--- src/modules/user/interface/user.routes.ts
+++ src/modules/user/interface/user.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { updateProfile } from "../interface/user.controller";
+import { authMiddleware } from "../../../core/middlewares/auth.middleware";
+
 
 const router = express.Router();
 
-router.put("/:id", updateProfile);
+router.put("/:id",authMiddleware, updateProfile);
 
 export default router;
